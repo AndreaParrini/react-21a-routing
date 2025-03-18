5d3717114f1dd0f0868a3fe3ab16da7205357ba8
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-18 10:49:39 +0100

    add new page to render error
    manage title and message conditionally

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,31 @@
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent'
+
 function ErrorPage(){
-    return <p>A error occurred!</p>
+
+    const error = useRouteError();
+
+    let title='An error occurred';
+    let message = 'Something went wrong';
+
+    if(error.status === 500){
+        message= JSON.parse(error.data).message;
+        //message= error.data.message; se si usa l'helper fornito da reract router json -> solo con versioni precedenti alla 7, ora ho installato la 7.30 e quindi non funziona
+    }
+
+    if(error.status === 404){
+        title= 'Not Found!';
+        message= 'Could not finde resource or page.'
+    }
+
+    return <>
+    <MainNavigation />
+    <PageContent title={title}>
+        <p>{message}</p>
+    </PageContent>
+    </>
+    
 }
 
 export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -20,11 +20,14 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/eventstg');
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return {isError: true, message: "Could not fetch events."}
-    throw {message: 'Could not fetch events.'}
+    throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {
+      status: 500
+    })
+   //throw json({message: 'Could not fetch events.'}, {status:500}) se si usa l'helper fornito da react router json() -> solo con versioni precedenti alla 7, ora ho installato la 7.30 e quindi non funziona
   } else {
     return response;
   }
