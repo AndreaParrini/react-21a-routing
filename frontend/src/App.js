8fad25961014787a4facbbd06a585df14aa6a612
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-19 10:32:20 +0100

    modify action to reuse it in the page of edit and a new event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,9 +26,10 @@ import RootEventsLayout from './pages/RootEvents';
 import HomePage from './pages/Home';
 import EventsPage, {loader as eventsLoader} from './pages/Events';
 import EventDetailPage, {loader as eventDetailLoader, action as eventDetailAction} from './pages/EventDetail';
-import NewEventPage, {action as newEventAction} from './pages/NewEvent';
+import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
+import {action as manipulateEventAction} from './components/EventForm';
 
 const router = createBrowserRouter([
   {
@@ -41,9 +42,9 @@ const router = createBrowserRouter([
         {index:true, element: <EventsPage />, loader: eventsLoader},
         {path:':eventId', id:'event-detail', loader:eventDetailLoader, children:[
           {index: true, element: <EventDetailPage />, action: eventDetailAction},
-          {path:'edit', element: <EditEventPage />},
+          {path:'edit', element: <EditEventPage />, action: manipulateEventAction},
         ]},
-        {path:'new', element: <NewEventPage />, action: newEventAction},
+        {path:'new', element: <NewEventPage />, action: manipulateEventAction},
       ]},
 
     ]
